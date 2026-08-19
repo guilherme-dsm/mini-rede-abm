d2805a9ea9db505b9de3fc36b3d0a8af76fbee4a
Guilherme dos Santos Mesquita <guilherme.smesquita07@gmail.com>
2026-08-18 23:30:28 -0300

    povoando-predios

--- seed_predios.py
+++ seed_predios.py
@@ -3,6 +3,9 @@ from models.db import get_connection, criar_tabelas
 PREDIOS = [
     ("Estela do Mar", "bloco 1"),
     ("Estela do Mar", "bloco 2"),
+    ("Barra D'Oro", "bloco 1"),
+    ("Mar de Prata", None),
+    ("Barra One", "bloco 1"),
 ]
 
 def seed():
